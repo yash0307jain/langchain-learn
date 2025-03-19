df68aabbe6e85f13337bd69d0846bfc6e3ae286f
Yash Jain <yash0307jain@gmail.com>
2025-03-19 11:17:17 +0530

    adding langchain memory and tools

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM python:3.11.11
 WORKDIR /app
 
 COPY requirements.txt /app
+COPY .env /app
 
 RUN pip install --upgrade pip
-RUN pip install --no-cache-dir streamlit
-#-r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY chatbot.py /app
 
--- chatbot.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Set page config
-st.set_page_config(page_title="Langchain Streamlit Basic Chatbot", page_icon="🙏")
-st.title("Chatbot using Streamlit and Langchain")
-
-prompt = st.chat_input("Say Something!")
-if prompt:
-    st.write(f"User has send the following {prompt}")
